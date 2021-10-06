86493010199d0f6a926003cce8dcd839da1debd0
Miryam <mbathilde@gmail.com>
2021-10-06 20:50:50 +0200

    ciclos de vida en task y TaskList con useEffect + useState

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LEVELS } from "../../models/levels.enum";
 import { Task } from '../../models/task.class';
 import TaskComponent from "../pure/task";
 
 const TaskList = () => {
-
+	
 	const defaultTask = new Task('Example', 'Default description', 'false', LEVELS.NORMAL);
+	
+	// definimos estado del componente a través de useState
+	const [tasks, setTasks] = useState([defaultTask]);
+	const [loading, setLoading] = useState(true); //por defecto SÍ que estará cargando
 
-	/* ChangeState = (id) => {
+	// definimos el control del ciclo de vida del componente
+	useEffect(() => {
+		console.log('Task state has been modified');
+		setLoading(false);
+		return () => {
+			console.log('TaskList component is going to unmount...');
+		};
+	}, [tasks]);
+
+
+	/* const changeCompleted = (id) => {
 		console.log('TODO: Cambiar estado de una tarea')
 	} */
 
--- src/components/pure/task.jsx
+++ src/components/pure/task.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Task } from '../../models/task.class';
 
 
 const TaskComponent = ({ task }) => {
+
+	useEffect(() => {
+		console.log('Task created');
+		return () => {
+			console.log(`Task: ${task.name} is going to unmount`)
+		};
+	}, [task]);
+
 	return (
 		<div>
 			<h2>
